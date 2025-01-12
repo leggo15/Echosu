ec027c0d5575fb90df771c5886c1bdbbbf31e5c0
Leggo15 <WildQard.no@gmail.com>
2025-01-12 07:17:10 +0100

    Update views.py

--- echo/views.py
+++ echo/views.py
@@ -941,7 +941,7 @@ def search_results(request):
                         (F('exact_match_distinct_count')*3.0 +
                         F('exact_match_total_count')*1.0 +
                         F('tag_match_count')*0.3)
-                        / (F('tag_miss_match_count') + 1 + (F('tag_surplus_count') * 0.2))
+                        / (F('tag_miss_match_count') * 0.3 + 1 + (F('tag_surplus_count') * 0.1))
                     )
                 )
                 .order_by('-tag_weight')
